ad76c503cd7a6a0bdc897efae2c0322a28618c08
Ali Hashemi <60406325+haashemi@users.noreply.github.com>
2023-11-13 23:25:53 +0330

    chore(cmd): change /arz text formatting

--- commands/arz.go
+++ commands/arz.go
@@ -1,30 +1,30 @@
 package commands
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/haashemi/tgo"
 	"github.com/haashemi/tgo/routers/message"
+	"golang.org/x/text/language"
+	printer "golang.org/x/text/message"
 )
 
 const ArzText = `💸| Exchange rates:
 
-<code>🇺🇸| USD:</code> %s T
-<code>🇪🇺| EUR:</code> %s T
-<code>🇬🇧| GBP:</code> %s T
-<code>🇹🇷| TRY:</code> %s T
-<code>🇷🇺| RUB:</code> %s T
+<code>🇺🇸| USD:</code> <code>%.0f IRT</code>
+<code>🇪🇺| EUR:</code> <code>%.0f IRT</code>
+<code>🇬🇧| GBP:</code> <code>%.0f IRT</code>
+<code>🇹🇷| TRY:</code> <code>%.0f IRT</code>
+<code>🇷🇺| RUB:</code> <code>%.0f IRT</code>
 
-<code>🪙| Emami     :</code> %s T
-<code>🪙| 1/1 Azadi :</code> %s T
-<code>🪙| 1/2 Azadi :</code> %s T
-<code>🪙| 1/4 Azadi :</code> %s T
-<code>🪙| Gerami    :</code> %s T
+<code>🪙| Emami     :</code> <code>%.0f IRT</code>
+<code>🪙| 1/1 Azadi :</code> <code>%.0f IRT</code>
+<code>🪙| 1/2 Azadi :</code> <code>%.0f IRT</code>
+<code>🪙| 1/4 Azadi :</code> <code>%.0f IRT</code>
+<code>🪙| Gerami    :</code> <code>%.0f IRT</code>
 
 ⏳| Last modification happened at %v
-
-🔥| Legal Advisers team.`
+`
 
 func (c *Commands) Arz(ctx *message.Context) {
 	data, err := c.bonbastClient.GetData()
@@ -41,7 +41,7 @@ func (c *Commands) Arz(ctx *message.Context) {
 	lastModifiedStr := lastModified.Local().Format(time.Stamp)
 
 	ctx.Reply(&tgo.SendMessage{
-		Text: fmt.Sprintf(ArzText, data.Usd1, data.Eur1, data.Gbp1, data.Try1, data.Rub1, data.Emami1, data.Azadi1, data.Azadi120, data.Azadi14, data.Azadi1G, lastModifiedStr),
+		Text: printer.NewPrinter(language.English).Sprintf(ArzText, data.Usd1, data.Eur1, data.Gbp1, data.Try1, data.Rub1, data.Emami1, data.Azadi1, data.Azadi120, data.Azadi14, data.Azadi1G, lastModifiedStr),
 
 		ReplyMarkup: tgo.InlineKeyboardMarkup{InlineKeyboard: [][]*tgo.InlineKeyboardButton{
 			{&tgo.InlineKeyboardButton{Text: "Source", Url: "https://bonbast.com/"}},
